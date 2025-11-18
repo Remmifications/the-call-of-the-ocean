271d74539a8678d53eeaa3501497d09a2d3300e4
Remmifications <remybird1@gmail.com>
2025-11-18 15:01:07 -0800

    Added an Assets folder and made 4 platforms of different tile types

--- Assets/scripts/h_box_container.gd
+++ Assets/scripts/h_box_container.gd
@@ -1,8 +1,8 @@
 extends HBoxContainer
 
-const WATER_HEALTH_FULL = preload("res://sprites/WaterHealth-full.png")
-const WATER_HEALTH_HALF = preload("res://sprites/WaterHealth-half.png")
-const WATER_HEALTH_EMPTY = preload("res://sprites/WaterHealth-empty.png")
+const WATER_HEALTH_FULL = preload("res://Assets/sprites/WaterHealth-full.png")
+const WATER_HEALTH_HALF = preload("res://Assets/sprites/WaterHealth-half.png")
+const WATER_HEALTH_EMPTY = preload("res://Assets/sprites/WaterHealth-empty.png")
 
 
 func update_partial(value):
