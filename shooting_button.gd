cc0298b00051a9cebeefae5da7ef9bbe3be657c6
Remmifications <remybird1@gmail.com>
2025-11-24 11:10:27 -0800

    Updated constants and shooting button

--- shooting_button.gd
+++ shooting_button.gd
@@ -2,17 +2,16 @@ extends TextureButton
 class_name Shooting_button
 
 # Icon found at https://game-icons.net/tags/gun.html
+@onready var time_label: Label = $Value
+@onready var cooldown = 0.5
 
-@onready var time_label = $Counter/Value
-
-@onready var cooldown = 0.5	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	time_label.hide()
-	$Sweep.value = 0
-	$Sweep.texture_progress = texture_normal
 	$Timer.wait_time = cooldown
+	time_label.hide()
+	$Sweep.texture_progress = texture_normal
+	$Sweep.value = 0
 	set_process(false)
 
 
@@ -20,7 +19,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	time_label.text = "%3.1f" % $Timer.time_left
 	$Sweep.value = int(($Timer.time_left / cooldown) * 100)
-
 
 
 func _on_pressed() -> void:
